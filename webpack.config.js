29a67930b7c0f6f1b2c79aeb0993de533ab2dafb
Casper <me@casperengelmann.com>
2019-11-28 21:30:18 +0100

    use webpack instead of typescript compiler

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const path = require('path');
+
+module.exports = {
+  entry: './src/try-catch.ts',
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'try-catch.js',
+    path: path.resolve(__dirname, 'lib'),
+  },
+};
