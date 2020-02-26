c8189cf374c654a68c3ca9896e06e14150ed5840
Casper <me@casperengelmann.com>
2020-02-26 20:53:46 +0100

    added lib files so no build is needed

---
+++ lib/try-catch.d.ts
@@ -0,0 +1,3 @@
+export declare function tryCatch<T>(subject: Function | Promise<T>, ...args: any): Promise<[Error, undefined] | (T | null)[]>;
+export default tryCatch;
+//# sourceMappingURL=try-catch.d.ts.map
