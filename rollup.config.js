e79ad04be2c8c128018e40725f42fddec828842a
Casper <me@casperengelmann.com>
2020-08-24 13:37:13 +0200

    reverted rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,37 +1,16 @@
 import typescript from '@rollup/plugin-typescript'
-import resolve from '@rollup/plugin-node-resolve'
-import commonjs from '@rollup/plugin-commonjs'
 import { terser } from 'rollup-plugin-terser'
 
-import pkg from './package.json'
-
-export default [
-  {
-    input: 'src/try-catch.ts',
-    output: {
-      name: 'tryCatch',
-      exports: 'named',
-      file: pkg.browser,
-      format: 'umd',
-    },
-    plugins: [
-      typescript(),
-      resolve(),
-      commonjs(),
-      terser(),
-    ],
+export default {
+  input: './src/try-catch.ts',
+  output: {
+    name: 'tryCatch',
+    dir: './lib',
+    format: 'umd',
+    sourcemap: true,
   },
-  {
-    input: 'src/try-catch.ts',
-    output: [
-      { file: pkg.main, format: 'cjs' },
-      { file: pkg.module, format: 'es' },
-    ],
-    plugins: [
-      typescript(),
-      resolve(),
-      commonjs(),
-      terser(),
-    ],
-  },
-]
+  plugins: [
+    typescript(),
+    terser(),
+  ],
+}
