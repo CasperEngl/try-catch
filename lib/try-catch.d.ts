cf13f4e240b168e94e14d98d09265552b3334d38
Casper <me@casperengelmann.com>
2020-08-24 04:38:23 +0200

    removed lib files

--- lib/try-catch.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function tryCatch<T>(subject: Function | Promise<T>, ...args: any): Promise<[Error, undefined] | (T | null)[]>;
-export default tryCatch;
-//# sourceMappingURL=try-catch.d.ts.map
