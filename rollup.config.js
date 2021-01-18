6b0bebed3bdebf16c02183b77b009c568f1aaa00
Casper Engelmann <me@casperengelmann.com>
2021-01-18 07:21:44 +0100

    upgrade: dependencies update: rollup config formatting

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,32 @@
 import typescript from '@rollup/plugin-typescript'
 import { terser } from 'rollup-plugin-terser'
 import resolve from '@rollup/plugin-node-resolve'
-import commonjs from '@rollup/plugin-commonjs'
+
+import pkg from './package.json'
 
 export default {
   input: './src/index.ts',
-  output: {
-    exports: 'named',
-    name: 'tryCatch',
-    dir: './lib',
-    format: 'umd',
-    sourcemap: true,
-  },
+  output: [
+    {
+      file: pkg.module,
+      format: 'es',
+      sourcemap: true,
+    },
+    {
+      file: pkg.main,
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      file: pkg.browser,
+      name: 'tryCatch',
+      format: 'umd',
+      sourcemap: true,
+    },
+  ],
   plugins: [
     typescript(),
     resolve(),
-    commonjs(),
     terser(),
   ],
 }
