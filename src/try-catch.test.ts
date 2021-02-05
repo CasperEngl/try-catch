cbc4e84426a7e0cdd7bc607079ddcbf7e3208214
Casper Engelmann <me@casperengelmann.com>
2021-02-05 18:34:56 +0100

    fix: import different build since node doesn't support esm

--- src/try-catch.test.ts
+++ src/try-catch.test.ts
@@ -10,7 +10,7 @@ import { ModelDefinition } from 'miragejs/-types';
 // eslint-disable-next-line import/no-unresolved
 import Schema from 'miragejs/orm/schema';
 
-import { tryCatch } from '../lib/try-catch.es';
+import { tryCatch } from '../lib/try-catch.umd';
 
 type User = {
   id: number;
