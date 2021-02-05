9a23a2cbaf95fffa489a98b9b671e5b62c326568
Casper Engelmann <me@casperengelmann.com>
2021-02-05 14:18:46 +0100

    feat: improve type inference

--- src/try-catch.ts
+++ src/try-catch.ts
@@ -5,38 +5,54 @@ export interface TryCatchError extends Error {
 }
 
 export class TryCatchError extends Error implements TryCatchError {
-  constructor(error: Error) { // esline-disable-error
-    super(error.name)
+  constructor(error: Error) {
+    super(error.name);
 
-    this.name = error.name
-    this.message = error.message
-    this.stack = error.stack
+    this.name = error.name;
+    this.message = error.message;
+    this.stack = error.stack;
   }
 }
 
-export async function tryCatch <T>(
-  subject: Function | Promise<T>,
+type TryCatchReturnType<ReturnType> =
+  | [TryCatchError, undefined]
+  | [null, ReturnType];
+
+export function tryCatch<ReturnType>(
+  subject: Promise<ReturnType>
+): Promise<TryCatchReturnType<ReturnType>>;
+export function tryCatch<ReturnType>(
+  subject: (..._args: any[]) => ReturnType
+): Promise<TryCatchReturnType<ReturnType>>;
+export function tryCatch<ReturnType>(
+  subject: (..._args: Promise<any>[]) => Promise<ReturnType>
+): Promise<TryCatchReturnType<ReturnType>>;
+export async function tryCatch<ReturnType>(
+  subject:
+    | Promise<ReturnType>
+    | ((..._args: any[]) => ReturnType)
+    | ((..._args: Promise<any>[]) => Promise<ReturnType>),
   ...args: any[]
-): Promise<[TryCatchError, undefined] | [null, T]> {
+): Promise<TryCatchReturnType<ReturnType>> {
   if (typeof subject === 'function') {
-    const fn = (subject as typeof subject)
+    const fn = subject as typeof subject;
 
     try {
-      return [null, await fn(...args)]
+      return [null, await fn(...args)];
     } catch (error) {
-      return [new TryCatchError(error), undefined]
+      return [new TryCatchError(error), undefined];
     }
   }
 
   if (Promise.resolve(subject) === subject) {
     try {
-      return [null, await subject]
+      return [null, await subject];
     } catch (error) {
-      return [new TryCatchError(error), undefined]
+      return [new TryCatchError(error), undefined];
     }
   }
 
-  return [new TypeError(`'${subject ? subject.toString() : subject}' is not a function or promise`), undefined]
+  return [new TypeError(`Subject is not a function or promise`), undefined];
 }
 
-export default tryCatch
+export default tryCatch;
