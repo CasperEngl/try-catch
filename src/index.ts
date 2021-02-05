16e92e231269ffcd9b3fb1567f2594219ce5787d
Casper Engelmann <me@casperengelmann.com>
2021-02-05 14:17:35 +0100

    feat: use prettier

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,17 @@
-import { tryCatch, TryCatchError } from './try-catch'
+import { tryCatch, TryCatchError } from './try-catch';
 
 declare global {
   interface Window {
     tryCatch: any;
   }
-
-  interface globalThis { // eslint-disable-line
-    [key: string]: any;
-  }
 }
-
 
 if (typeof window !== 'undefined') {
-  window.tryCatch = tryCatch
+  window.tryCatch = tryCatch;
 } else if (typeof globalThis !== 'undefined') {
-  (globalThis as any).tryCatch = tryCatch
+  (globalThis as any).tryCatch = tryCatch;
 }
 
-export {
-  tryCatch,
-  TryCatchError,
-}
+export { tryCatch, TryCatchError };
 
-export default tryCatch
+export default tryCatch;
