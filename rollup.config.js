9b4edde9d0965f42bb4d9043b7648da34982d665
Casper Engelmann <me@casperengelmann.com>
2021-02-05 17:30:27 +0100

    fix: extensions + types

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import typescript from '@rollup/plugin-typescript';
 import { terser } from 'rollup-plugin-terser';
 import resolve from '@rollup/plugin-node-resolve';
 import cleanup from 'rollup-plugin-cleanup';
+import copy from 'rollup-plugin-copy';
 
 import pkg from './package.json';
 
@@ -9,18 +10,23 @@ export default {
   input: './src/index.ts',
   output: [
     {
+      name: 'tryCatch',
+      exports: 'named',
       file: pkg.module,
       format: 'es',
       sourcemap: true,
     },
     {
+      name: 'tryCatch',
+      exports: 'named',
       file: pkg.main,
       format: 'cjs',
       sourcemap: true,
     },
     {
-      file: pkg.browser,
       name: 'tryCatch',
+      exports: 'named',
+      file: pkg.browser,
       format: 'umd',
       sourcemap: true,
     },
@@ -31,6 +37,25 @@ export default {
     terser(),
     cleanup({
       comments: 'none',
+    }),
+    copy({
+      targets: [
+        {
+          src: './lib/try-catch.d.ts',
+          dest: './lib',
+          rename: 'try-catch.es.d.ts',
+        },
+        {
+          src: './lib/try-catch.d.ts',
+          dest: './lib',
+          rename: 'try-catch.cjs.d.ts',
+        },
+        {
+          src: './lib/try-catch.d.ts',
+          dest: './lib',
+          rename: 'try-catch.umd.d.ts',
+        },
+      ],
     }),
   ],
 };
--- src/try-catch.test.ts
+++ src/try-catch.test.ts
@@ -10,7 +10,7 @@ import { ModelDefinition } from 'miragejs/-types';
 // eslint-disable-next-line import/no-unresolved
 import Schema from 'miragejs/orm/schema';
 
-import { tryCatch } from '../lib/try-catch';
+import { tryCatch } from '../lib/try-catch.es';
 
 type User = {
   id: number;
