30cb2b9cd6118a7f92ccea194f29a110e00768da
Casper Engelmann <me@casperengelmann.com>
2021-02-09 17:29:41 +0100

    added: more builds

--- rollup.config.js
+++ rollup.config.js
@@ -1,53 +1,101 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import typescript from '@rollup/plugin-typescript';
-import { terser } from 'rollup-plugin-terser';
 import resolve from '@rollup/plugin-node-resolve';
 import cleanup from 'rollup-plugin-cleanup';
+import sourcemaps from 'rollup-plugin-sourcemaps';
 import copy from 'rollup-plugin-copy';
+import { terser } from 'rollup-plugin-terser';
+
+import { omit } from 'lodash';
 
 import pkg from './package.json';
 
-export default {
-  input: './src/index.ts',
-  output: [
-    {
-      name: 'tryCatch',
-      exports: 'named',
-      file: pkg.module,
-      format: 'es',
-      sourcemap: true,
+const name = 'tryCatch';
+
+const options = [
+  {
+    name,
+    exports: 'named',
+    file: pkg.module,
+    format: 'es',
+    env: 'production',
+    sourcemap: true,
+  },
+  {
+    name,
+    exports: 'named',
+    file: pkg.module,
+    format: 'es',
+    env: 'development',
+    sourcemap: true,
+  },
+  {
+    name,
+    exports: 'named',
+    file: pkg.main,
+    format: 'cjs',
+    env: 'production',
+    sourcemap: true,
+  },
+  {
+    name,
+    exports: 'named',
+    file: pkg.main,
+    format: 'cjs',
+    env: 'development',
+    sourcemap: true,
+  },
+  {
+    name,
+    exports: 'named',
+    file: pkg.browser,
+    format: 'umd',
+    env: 'production',
+    sourcemap: true,
+  },
+  {
+    name,
+    exports: 'named',
+    file: pkg.browser,
+    format: 'umd',
+    env: 'development',
+    sourcemap: true,
+  },
+];
+
+function createRollupConfig(option) {
+  const file = option.env
+    ? `${option.file.replace('.js', '')}.${option.env}.js`
+    : option.file;
+  const fileDt = option.env
+    ? `${option.file.replace('.js', '')}.${option.env}.d.ts`
+    : option.file;
+
+  return {
+    input: './src/index.ts',
+    output: {
+      ...omit(option, ['env']),
+      file,
     },
-    {
-      name: 'tryCatch',
-      exports: 'named',
-      file: pkg.main,
-      format: 'cjs',
-      sourcemap: true,
-    },
-    {
-      name: 'tryCatch',
-      exports: 'named',
-      file: pkg.browser,
-      format: 'umd',
-      sourcemap: true,
-    },
-  ],
-  plugins: [
-    typescript(),
-    resolve(),
-    terser(),
-    cleanup({
-      comments: 'none',
-    }),
-    copy({
-      targets: [
-        'try-catch.es.d.ts',
-        'try-catch.cjs.d.ts',
-        'try-catch.umd.d.ts',
-      ].map((typeDef) => ({
-        src: './lib/try-catch.d.ts',
-        dest: './lib',
-        rename: typeDef,
-      })),
-    }),
-  ],
-};
+    plugins: [
+      typescript(),
+      resolve(),
+      cleanup({
+        comments: 'none',
+      }),
+      sourcemaps(),
+      option.env === 'production' && terser(),
+      copy({
+        targets: [
+          {
+            src: './lib/try-catch.d.ts',
+            dest: '.',
+            rename: fileDt,
+          },
+        ],
+      }),
+    ],
+  };
+}
+
+export default options.map((option) => createRollupConfig(option));
